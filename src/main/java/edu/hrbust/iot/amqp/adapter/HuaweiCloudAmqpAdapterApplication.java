6bc521a4941c6827de9222b8cc292da06f5367ae
WWWWWWWXQ <matthewweasley@163.com>
2021-04-28 14:19:31 +0800

    init repository

--- src/main/java/edu/hrbust/iot/amqp/adapter/HuaweiCloudAmqpAdapterApplication.java
+++ src/main/java/edu/hrbust/iot/amqp/adapter/HuaweiCloudAmqpAdapterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HuaweicloudAmqpAdapterApplication {
+public class HuaweiCloudAmqpAdapterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HuaweicloudAmqpAdapterApplication.class, args);
+        SpringApplication.run(HuaweiCloudAmqpAdapterApplication.class, args);
     }
 
 }
