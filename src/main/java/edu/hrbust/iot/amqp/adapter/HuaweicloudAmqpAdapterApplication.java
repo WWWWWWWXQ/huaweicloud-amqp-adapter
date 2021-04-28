dfdbe20f3f038b2d85ae164f0c9fdc9bcef8e084
WWWWWWWXQ <matthewweasley@163.com>
2021-04-28 14:02:05 +0800

    Initial commit

---
+++ src/main/java/edu/hrbust/iot/amqp/adapter/HuaweicloudAmqpAdapterApplication.java
@@ -0,0 +1,13 @@
+package edu.hrbust.iot.amqp.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaweicloudAmqpAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuaweicloudAmqpAdapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hrbust/iot/amqp/adapter/HuaweicloudAmqpAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hrbust.iot.amqp.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuaweicloudAmqpAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
